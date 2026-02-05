b525ce343a18fd68de9eb8376964d3ba356cccea
song-juhyun <juju020716@gmail.com>
2026-02-06 00:28:17 +0900

    Edit init.lua to support both Linux/Mac.

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-vim.opt.runtimepath:append("/home/songjuhyeon/.config/nvim/pack/plugins/start/nvim-treesitter")
-vim.opt.runtimepath:append("/home/songjuhyeon/.config/nvim/pack/plugins/start/nvim-dap")
-vim.opt.runtimepath:append("/home/songjuhyeon/.config/nvim/pack/plugins/start/nvim-dap-ui")
-vim.opt.runtimepath:append("/home/songjuhyeon/.config/nvim/pack/plugins/start/nvim-dap-virtual-text")
+local plugin_dir = vim.fn.stdpath('config') .. "/pack/plugins/start/"
 
-
+vim.opt.runtimepath:append(plugin_dir .. "nvim-treesitter")
+vim.opt.runtimepath:append(plugin_dir .. "nvim-dap")
+vim.opt.runtimepath:append(plugin_dir .. "nvim-dap-ui")
+vim.opt.runtimepath:append(plugin_dir .. "nvim-dap-virtual-text")
 
 vim.opt.number = true
 require("treesitter_setup")
